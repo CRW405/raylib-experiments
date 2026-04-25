45a6e888f6c0a43753b52b9511cb4255753ea7d1
CRW405 <134819932+CRW405@users.noreply.github.com>
2026-04-24 22:32:27 -0500

    added camera to mapper

--- mapper.c
+++ mapper.c
@@ -17,10 +17,16 @@
 // Color Support
 
 // NOTES:
-// https://www.raylib.com/examples.html
+//
+
+// TODO:
+//
 
 #include "raylib.h"
+#include "raymath.h"
+#include "rlgl.h"
 #include <math.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
 	// window setup
@@ -37,7 +43,49 @@ int main(int argc, char *argv[]) {
 
 	// main loop
 	while (!WindowShouldClose()) {
-	}
+		// controls
+		// cam movement
+		if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+			Vector2 delta = GetMouseDelta();
+			delta = Vector2Scale(delta, -1.0f / cam.zoom);
+			cam.target = Vector2Add(cam.target, delta);
+		}
+		// TODO: Arrow key, WASD, Vi movement
 
+		// zoom
+		float wheel = GetMouseWheelMove();
+		if (wheel != 0) {
+			Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), cam);
+			cam.offset = GetMousePosition();
+			cam.target = mouseWorldPos;
+			float scale = wheel;
+			cam.zoom = Clamp(expf(logf(cam.zoom) + (scale / 5)), 0.1f, 100.0f);
+		}
+		// TODO: Right click drag zoom
+		// TODO: CTRL -/+ zoom, CTRL Up Down, CTRL JK zoom
+
+		// render
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+		BeginMode2D(cam);
+
+		// draw grid
+		// TODO: make grid infinite
+		rlPushMatrix();
+		rlTranslatef(0, 25 * 50, 0);
+		rlRotatef(90, 1, 0, 0);
+		DrawGrid(500, 10);
+		rlPopMatrix();
+		EndMode2D();
+
+		Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), cam);
+		DrawTextEx(GetFontDefault(),
+		           TextFormat("%.0f, %.0f", mouseWorldPos.x, mouseWorldPos.y),
+		           Vector2Add(GetMousePosition(), (Vector2){-44, -24}), 20, 2,
+		           BLACK);
+
+		EndDrawing();
+	}
+	CloseWindow();
 	return EXIT_SUCCESS;
 }
